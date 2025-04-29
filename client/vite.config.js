a3f303e3e6c9712a1a1944529d01b21494bc28cc
rushabh <rushabh.ingle2111@gmail.com>
2025-04-29 22:39:42 +0530

    config changes

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,4 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // Base public path when served in production
+  base: '/',
+  // Build options
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+  },
 })
